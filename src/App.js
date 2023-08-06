a5fcda7e4d16c7d28fcaa0ad0e642db15cfd82e7
developer-abdulali <ab.ali.soomro@gmail.com>
2023-08-06 17:56:36 +0500

    added some changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios"; // Import axios using ES6 module syntax
 import "./App.css";
 
 function App() {
+  const [options, setOptions] = useState([]);
+  const [to, setTo] = useState("en");
+  const [from, setFrom] = useState("en");
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
+  const translate = () => {
+    axios
+      .post(
+        "https://libretranslate.de/translate",
+        {
+          q: input,
+          source: from,
+          target: to,
+          api_key: "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+        },
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        setOutput(res.data.translatedText);
+      })
+      .catch((error) => {
+        console.error("Translation error:", error);
+      });
+  };
+
   useEffect(() => {
     axios
       .get("https://libretranslate.de/languages", {
         headers: { Accept: "application/json" },
       })
       .then((res) => {
-        console.log(res);
+        console.log(res.data);
+        setOptions(res.data);
       })
       .catch((error) => {
         console.error("Error fetching languages:", error);
@@ -19,24 +51,34 @@ function App() {
   return (
     <div className="App">
       <div>
-        From:
-        <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
+        From ({from}) :
+        <select onChange={(e) => setFrom(e.target.value)}>
+          {options.map((opt) => (
+            <option key={opt.code} value={opt.code}>
+              {opt.name}
+            </option>
+          ))}
         </select>
-        To:
-        <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
+        To ({to}) :
+        <select onChange={(e) => setTo(e.target.value)}>
+          {options.map((opt) => (
+            <option key={opt.code} value={opt.code}>
+              {opt.name}
+            </option>
+          ))}
         </select>
         <div>
-          <textarea name="" id="" cols="50" rows="8"></textarea>
+          <textarea
+            cols="50"
+            rows="8"
+            onInput={(e) => setInput(e.target.value)}
+          ></textarea>
         </div>
         <div>
-          <textarea name="" id="" cols="50" rows="8"></textarea>
+          <textarea name="" id="" cols="50" rows="8" value={output}></textarea>
         </div>
         <div>
-          <button>Translate</button>
+          <button onClick={(e) => translate()}>Translate</button>
         </div>
       </div>
     </div>
